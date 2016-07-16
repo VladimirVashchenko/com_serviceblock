c86af68016eceae9a1196ca613df108d67e0b4a6
VladimirVashchenko <vashchenko.vs.88@gmail.com>
2016-07-16 14:47:54 +0300

    lang

--- admin/views/serviceblocks/tmpl/default.php
+++ admin/views/serviceblocks/tmpl/default.php
@@ -28,7 +28,7 @@ $listDirn = $this->escape($this->state->get('list.direction'));
             <?php endif; ?>
             <div class="row-fluid">
                 <div class="span6">
-                    <?php echo JText::_('COM_SERVICEBLOCK_SERVICEBLOCKS_FILTER'); ?>
+                    <?php echo JText::_('COM_SERVICEBLOCKS_FILTER'); ?>
                     <?php
                     echo JLayoutHelper::render(
                         'joomla.searchtools.default',
@@ -48,10 +48,10 @@ $listDirn = $this->escape($this->state->get('list.direction'));
                         <?php echo JHtml::_('grid.sort', 'JSTATUS', 'a.state', $listDirn, $listOrder); ?>
                     </th>
                     <th width="60%">
-                        <?php echo JHtml::_('grid.sort', 'COM_SERVICEBLOCK_SERVICEBLOCKS_NAME', 'heading', $listDirn, $listOrder); ?>
+                        <?php echo JHtml::_('grid.sort', 'COM_SERVICEBLOCKS_NAME', 'heading', $listDirn, $listOrder); ?>
                     </th>
                     <th width="25%">
-                        <?php echo JHtml::_('grid.sort', 'COM_SERVICEBLOCK_SERVICEBLOCK_FIELD_CATID_LABEL', 'title', $listDirn, $listOrder); ?>
+                        <?php echo JHtml::_('grid.sort', 'COM_SERVICEBLOCK_FIELD_CATID_LABEL', 'title', $listDirn, $listOrder); ?>
                     </th>
                     <th width="1%">
                         <?php echo JHtml::_('grid.sort', 'COM_SERVICEBLOCK_ID', 'id', $listDirn, $listOrder); ?>
